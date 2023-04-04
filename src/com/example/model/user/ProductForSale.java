085537b0b12085fdaa957cb6f0f1e6f9ae2a6835
bi151103 <93708621+bi151103@users.noreply.github.com>
2023-04-04 21:33:03 +0700

    Add files via upload
    upload src file

---
+++ src/com/example/model/user/ProductForSale.java
@@ -0,0 +1,43 @@
+package com.example.model.user;
+import com.example.*;
+
+import java.util.*;
+public class ProductForSale extends Product {
+    public ProductForSale(String image, String description, String name, String category, int quantity) {
+        this.image = image;
+        this.description = description;
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+    }
+    class CustomerReview {
+        public CustomerReview(int noOfStarts, String review) {
+            this.noOfStars = noOfStarts;
+            this.review = review;
+        }
+        private int noOfStars;
+        private String review;
+        public int getNoOfStarts() {return noOfStars;}
+        public String getReview() {return review;}
+    }
+    private int quantity;
+    private int unitPrice;
+    private ArrayList<CustomerReview> crs;
+    private int noOfSolds;
+    public int getQuantity() {return quantity;}
+    public int getUnitPrice() {return unitPrice;}
+    public int getNoOfSolds() {return noOfSolds;}
+    public ArrayList<CustomerReview> getReviews() {return crs;}
+    public void addCustomerReview(CustomerReview cr) {
+        this.crs.add(cr);
+    }
+    public static void updateNoOfSolds(User.CartState cartState) {
+        int size = cartState.products.size();
+        for(int i = 0; i < size; i++) {
+            cartState.products.get(i).noOfSolds += cartState.quantityEachProduct.get(i);
+        }
+    }
+    public void updateQuantity(int quantity) {
+        this.quantity += quantity;
+    }
+}
